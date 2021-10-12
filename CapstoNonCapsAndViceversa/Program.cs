81733cbdc9b5101dcbf8447875e750d29559ff57
vishal.Alok <you@example.com>
2021-10-13 02:28:31 +0530

    Add project files.

---
+++ CapstoNonCapsAndViceversa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CapstoNonCapsAndViceversa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            //string[] sArray = s.Split("");
+            char[] cArray = s.ToCharArray();
+
+            string FinalString = string.Empty;
+            foreach (char item in cArray)
+            {
+                string Convertedstring = Convert.ToString(item);
+                if (char.IsUpper(item))
+                {
+                    Convertedstring = Convertedstring.ToLower();
+                }
+                else
+                {
+                    Convertedstring = Convertedstring.ToUpper();
+                }
+                FinalString += Convertedstring;
+            }
+            Console.WriteLine(FinalString);
+            Console.ReadLine();
+        }
+    }
+}
